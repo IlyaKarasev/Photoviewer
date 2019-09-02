f1cc92414c3bb06363c87ea9710a804990505f4f
ILYA Karasev <karasev.los@yandex.ru>
2019-09-02 16:04:24 +0300

    Create UI

---
+++ Photoviewer/Controllers/PhotosController.swift
@@ -0,0 +1,41 @@
+//
+//  PhotosController.swift
+//  Photoviewer
+//
+//  Created by ILYA Karasev on 02/09/2019.
+//  Copyright © 2019 Ilya Karasev. All rights reserved.
+//
+
+import UIKit
+
+class PhotosController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 0
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCell.reuseId, for: indexPath)
+    
+        // Configure the cell
+    
+        return cell
+    }
+    
+}
